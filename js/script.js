7896e428f13143ddbb1b07382d576074ffa8ec84
janithbro <janithjayasanka1000@gmail.com>
2023-07-12 13:47:58 +0530

    website is now responsible

--- js/script.js
+++ js/script.js
@@ -61,3 +61,41 @@ circles.forEach(elem=>{
 //mix it up portfolio section
 var mixer = mixitup('.portfolio-gallery');
 
+// active menu ////////////////////
+let menuli = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuli.forEach(sec => sec.classList.remove("active"));
+    menuli[len].classList.add('active');
+}
+
+activeMenu();
+window.addEventListener("scroll",activeMenu);
+
+
+// sticky navbar ////////////////////
+const header = document.querySelector("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",this.window.scrollY > 50)
+})
+
+// toggle icon navbar ////////////////////
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+
+window.onscroll = ()=>{
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.remove("open");
+}
+
+
+// parallax ////////////////////
+
